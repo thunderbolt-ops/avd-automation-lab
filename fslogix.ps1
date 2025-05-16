c473af4e31ef353aa7dd5fe007cbd15d541c448f
thunderbolt-ops <itpro0707@gmail.com>
2025-05-16 22:43:32 +0530

    Update fslogix.ps1

--- fslogix.ps1
+++ fslogix.ps1
@@ -1,1 +1,13 @@
+# fslogix.ps1 (dynamic version)
+param(
+    [string]$FslogixSharePath
+)
+
+New-Item -Path "HKLM:\\SOFTWARE\\FSLogix" -Force | Out-Null
+New-Item -Path "HKLM:\\SOFTWARE\\FSLogix\\Profiles" -Force | Out-Null
+
+Set-ItemProperty -Path "HKLM:\\SOFTWARE\\FSLogix\\Profiles" -Name "Enabled" -Value 1 -Type DWord
+Set-ItemProperty -Path "HKLM:\\SOFTWARE\\FSLogix\\Profiles" -Name "VHDLocations" -Value $FslogixSharePath
+Set-ItemProperty -Path "HKLM:\\SOFTWARE\\FSLogix\\Profiles" -Name "IsDynamic" -Value 1 -Type DWord
+Set-ItemProperty -Path "HKLM:\\SOFTWARE\\FSLogix\\Profiles" -Name "VolumeType" -Value "vhdx"
 
