c695218aef6109535cddc160ad4f92b86c6f0ea0
thunderbolt-ops <itpro0707@gmail.com>
2025-05-20 13:51:40 +0530

    Update promote-to-dc.ps1

--- promote-to-dc.ps1
+++ promote-to-dc.ps1
@@ -1,1 +1,14 @@
 
+$domainName = "avdlab.local"
+$netbiosName = "AVDLAB"
+$safeModePwd = "YourP@ssw0rd123!"
+
+# Install AD Domain Services
+Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
+
+# Promote to Domain Controller
+Install-ADDSForest `
+    -DomainName $domainName `
+    -DomainNetbiosName $netbiosName `
+    -SafeModeAdministratorPassword (ConvertTo-SecureString $safeModePwd -AsPlainText -Force) `
+    -Force:$true
